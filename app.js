1ad4097eace9e062186f7d5d4d179b5545d4d37a
Gilberto Méndez <gilberto.mendez.santiz@gmail.com>
2017-10-13 17:13:27 -0500

    app ready to upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const stripe = require('stripe')('sk_test_rzEaINjrKj6syFfkUXq3pU4y')
+const keys = require('./config/keys')
+const stripe = require('stripe')(keys.stripeSecretKey)
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 
@@ -14,9 +15,31 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 //set static folder
-app.use(express.static(`${__diname}/public`))
+app.use(express.static(`${__dirname}/public`))
 
-
+app.get('/', (req, res) => {
+    res.render('index',{
+        stripePublishableKey: keys.stripePublishableKey
+    })
+})
+app.post('/charge', (req, res) => {
+    const amount = 250 * 100
+    stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+    })
+    .then((customer) => {
+        stripe.charges.create({
+            amount,
+            description: 'new book to porgrammers',
+            currency: 'mxn',
+            customer: customer.id
+        })
+        .then(charge => res.render('success'))
+    })
+    console.log(req.body)
+    
+})
 app.listen(port, () => {
     console.log("Server running  on port :" + port)
 })
